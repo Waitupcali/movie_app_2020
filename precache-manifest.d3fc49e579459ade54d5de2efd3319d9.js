60d40bb76dbdb48e2cde48c28f679375ce12c6f2
Waitupcali <jihyeok2yo@gmail.com>
2020-06-28 00:20:12 +0900

    Updates

---
+++ precache-manifest.d3fc49e579459ade54d5de2efd3319d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8e3a30f0a7d7df3f2f5d5696e42d9c8",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "41861371539d5d0adc95",
+    "url": "/movie_app_2020/static/css/main.10b0ebdd.chunk.css"
+  },
+  {
+    "revision": "b4ad6d5796d4c9c3f3c1",
+    "url": "/movie_app_2020/static/js/2.3507762e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.3507762e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41861371539d5d0adc95",
+    "url": "/movie_app_2020/static/js/main.db53b438.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
